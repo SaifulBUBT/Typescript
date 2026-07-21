f0c54516f37bf1f31725ec25d70c3b4e4877b9c9
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2026-07-22 03:51:35 +0600

    type annotate for premitives, array, objects

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,31 @@ function greet(name, msg) {
     return `${msg} ${name}!`;
 }
 console.log(greet("How are you?", "Saiful Islam"));
+let a = 10;
+console.log(a);
+const names = ["Saiful", "Alex", "John"];
+names.push("Doe");
+console.log(names);
+const rolls = [1, 2, 3, 4, 5];
+const person = {
+    name: "Saiful Islam",
+    age: 30,
+};
+console.log(person);
+const config = {
+    appName: "MyApp",
+    version: "1.0.0",
+    debug: true,
+    port: 3000,
+    database: {
+        host: "localhost",
+        port: 5432,
+        username: "admin",
+        password: "password",
+    },
+    features: {
+        registration: true,
+        darkMode: false,
+    },
+};
+console.log(config);
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,64 @@ function greet(name: string, msg: string): string {
 }
 
 console.log(greet("How are you?", "Saiful Islam"));
+
+let a: number = 10;
+console.log(a);
+
+const names: string[] = ["Saiful", "Alex", "John"];
+
+names.push("Doe");
+console.log(names);
+
+const rolls: number[] = [1, 2, 3, 4, 5];
+
+// Objects
+interface Person {
+  name: string;
+  age: number;
+}
+
+const person: Person = {
+  name: "Saiful Islam",
+  age: 30,
+};
+console.log(person);
+
+// using type
+
+type DatabaseConfig = {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+};
+
+type AppConfig = {
+  appName: string;
+  version: string;
+  debug: boolean;
+  port: number;
+  database: DatabaseConfig;
+  features: {
+    registration: boolean;
+    darkMode: boolean;
+  };
+};
+
+const config: AppConfig = {
+  appName: "MyApp",
+  version: "1.0.0",
+  debug: true,
+  port: 3000,
+  database: {
+    host: "localhost",
+    port: 5432,
+    username: "admin",
+    password: "password",
+  },
+  features: {
+    registration: true,
+    darkMode: false,
+  },
+};
+console.log(config);
